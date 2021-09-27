5195a27831856fe46cb6141b8ac9c38f572944c5
Travis Vachon <travis.vachon@gmail.com>
2021-09-26 22:12:21 -0700

    minor avatar tweaks

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -1,5 +1,5 @@
 export default function Avatar({ src, className }) {
   return (
-    <img src={src} className={`w-12 h-12 rounded-full ring ring-2 ring-white`} />
+    <img src={src} className={`w-12 h-12 rounded-full border-2 border-white ${className}`} />
   )
 }
--- stories/Avatar.stories.jsx
+++ stories/Avatar.stories.jsx
@@ -8,7 +8,7 @@ export default {
 }
 
 export const StandardAvatar = () => (
-  <Avatar src={avatarImg}/>
+  <Avatar src={avatarImg} />
 )
 StandardAvatar.parameters = {
   backgrounds: { default: "dark" }
