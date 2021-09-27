2d698f928374c231b2578311bb7403478692d271
Ian Davis <jungziege@gmail.com>
2021-09-27 12:32:04 -0700

    Edit Avatar component to show defualt if no src

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -1,5 +1,24 @@
-export default function Avatar({ className, ...rest }) {
-  return (
-    <img className={`rounded-full border-2 border-white ${className}`} {...rest} />
-  )
+export default function Avatar({ src, className }) {
+  if (src) {
+    return (
+      <img
+        src={src}
+        className={`w-12 h-12 rounded-full border-2 border-white ${className}`}
+      />
+    );
+  } else {
+    return (
+      <span
+        className={`inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100 cursor-pointer ${className}`}
+      >
+        <svg
+          className="h-full w-full text-gray-300"
+          fill="currentColor"
+          viewBox="0 0 24 24"
+        >
+          <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+        </svg>
+      </span>
+    );
+  }
 }
