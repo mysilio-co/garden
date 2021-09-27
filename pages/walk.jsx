b7f6de3ba97c5d8af0c713beab4c64ee4373934a
Ian Davis <jungziege@gmail.com>
2021-09-26 17:21:48 -0700

    Add basic page for Garden Walks

---
+++ pages/walk.jsx
@@ -0,0 +1,9 @@
+export default function GardenWalk() {
+  return (
+    <div className="page" id="page">
+      <div className="text-center">
+        Go on a walk through your garden or Create a new Map
+      </div>
+    </div>
+  );
+}
