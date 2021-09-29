3056cbe8a0b48fb6d38b24fa20c19de28c6cd4b5
Travis Vachon <travis.vachon@gmail.com>
2021-09-28 21:43:17 -0700

    Use TailwindUI example to style modal
    Make this look and feel great using the example at https://tailwindui.com/components/application-ui/overlays/modals

--- stories/NewNoteModal.stories.jsx
+++ stories/NewNoteModal.stories.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Dialog } from '@headlessui/react'
+import React, { Fragment, useState } from 'react';
+import { Dialog, Transition } from '@headlessui/react'
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import NewNote from '../components/modals/NewNote';
@@ -32,15 +32,45 @@ export function ModalNewNoteModal() {
       <button className="btn" onClick={() => setIsOpen(!isOpen)}>
         Show Modal
       </button>
-      <Dialog open={isOpen} onClose={() => setIsOpen(false)}
-        className="fixed z-10 inset-0 overflow-y-auto"
-      >
-        <Dialog.Overlay className="fixed z-0 inset-0 bg-black opacity-30" />
+      <Transition.Root show={isOpen} as={Fragment}>
+        <Dialog as="div" onClose={() => setIsOpen(false)}
+          className="fixed z-10 inset-0 overflow-y-auto"
+        >
+          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
 
-        <div className="relative bg-white rounded max-w-sm mx-auto">
-          <NewNote onClose={() => setIsOpen(false)}/>
-        </div>
-      </Dialog>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0 bg-black opacity-75 transition-opacity" />
+            </Transition.Child>
+
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+              &#8203;
+            </span>
+
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm ">
+                <NewNote onClose={() => setIsOpen(false)} />
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition.Root>
     </>
   )
 }
