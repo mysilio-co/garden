7847db1384218a09ef4373dcc6339842be2b0da7
Ian Davis <jungziege@gmail.com>
2021-10-04 19:52:08 -0700

    ImageMap roughly working, looks like shit

--- pages/demo.jsx
+++ pages/demo.jsx
@@ -1,64 +1,147 @@
-import { useState } from "react";
-import { Walker, HexMap, HexTile, Direction, Orientation } from "../model/hex";
-import Nav from "../components/nav";
+import Link from 'next/link'
+import { Loader } from '../components/elements'
+import Nav from '../components/nav'
 
-const Colors = [
-  "bg-passionflower",
-  "bg-lagoon",
-  "bg-echeveria",
-  "bg-chanterelle",
-  "bg-coral",
-  "bg-ember",
-  "bg-ocean",
-  "bg-aloe",
-  "bg-kelp",
-  "bg-apricot",
-  "bg-salmon",
-];
+const scale = 1 / 3;
+const H = 1950 * scale;
+const W = 4050 * scale;
 
-const DemoImages = [
-  {
-    name: "sagfam",
-    src: "https://www.lapedrera.com/sites/default/files/2019-11/home-la-pedrera-terrat-azotea-rooftop.jpg",
-    areas: [
-      {
-        url: "https://cdn.casabatllo.es/wp-content/uploads/2018/01/fachada-590x760.png",
-        coords: [0, 0, 600, 600],
-      },
-      {
-        url: "https://www.catalannews.com/images/cna/images/2020/09/sagfam.jpg",
-        coords: [600, 600, 1200, 1200],
-      },
-    ],
-  },
-];
+export const StoragePrefix =
+  "https://ian.myunderstory.com/public/demo/ImageMap/demo/";
+export const DemoPath = "/demo";
+export const DemoPrefix = `${DemoPath}/`;
 
+export const MountainDashboard = {
+  src: `${StoragePrefix}00-mountains-dashboard.jpg`,
+  areas: [
+    {
+      href: `${DemoPrefix}vision`,
+      coords: [0, 0, W, H/3],
+    },
+    {
+      href: `${DemoPrefix}pitch`,
+      coords: [0, (H * 2) / 3, W, H],
+    },
+    {
+      href: `${DemoPrefix}library`,
+      coords: [0, H / 3, W, (H * 2) / 3],
+    },
+  ],
+};
 
-export function AreaLink({ coords,  url }) {
-  const [x1,x2,y1,y2] = coords
-  return <area shape="rect" coords={`${x1},${y1},${x2},${y2}`} href={url} />;
+export const PathPitch = {
+  src: `${StoragePrefix}01-path-pitch.jpg`,
+  areas: [
+    {
+      href: DemoPath,
+      coords: [0, 0, W, H],
+    },
+  ],
+};
+export const ForestLibrary = {
+  src:`${StoragePrefix}02-forest-library.jpg`,
+  areas: [
+    {
+      href: DemoPath,
+      coords: [0, 0, W, H],
+    },
+  ],
+ }
+
+export const SkyVision = {
+  src:`${StoragePrefix}03-sky-vision.jpg`,
+  areas: [
+    {
+      href: DemoPath,
+      coords: [0, 0, W, H],
+    },
+  ],
+ }
+
+export const DemoImageMaps = {
+  dashboard: MountainDashboard,
+  pitch: PathPitch,
+  library: ForestLibrary,
+  vision: SkyVision,
 }
+
+function useWindowSize() {
+  // taken from: https://usehooks.com/useWindowSize/
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+
+  return windowSize;
+}
+
+export function AreaLink({ coords, href}) {
+  const [x1, y1, x2, y2] = coords;
+  return (
+    <Link href={href}>
+      <a
+        className={`bg-storm absolute top-${y1} left-${x1} block`}
+        height={y2 - y1}
+        width={x2 - x1}
+        href={href}
+      >
+        <area shape="rect" coords={`${x1},${y1},${x2},${y2}`} href={href} />
+      </a>
+    </Link>
+  );
+ }
 
 export function ImageMap({ name, src, areas }) {
   return (
-    <>
+    <div className="flex flex-col justify-center items-center h-full">
       <map name={name}>
-        {areas.map(({ coords, url }) => (
-          <AreaLink coords={coords} url={url} />
+        {areas.map(({ coords, href }) => (
+          <AreaLink coords={coords} href={href} />
         ))}
       </map>
-      <img usemap={`#${name}`} src={src} />
-    </>
+      <img
+        width={W}
+        height={H}
+        usemap={`#${name}`}
+        src={src}
+      />
+    </div>
   );
 }
 
-export default function Demo() {
+export function Demo({name="dashboard"}) {
+  console.log(name)
+  const {src, areas} = DemoImageMaps[name]
   return (
-    <div className="page" id="page">
+    <div className="page" id="page" className="bg-aquamarine">
       <Nav />
-      {DemoImages.map(({ name, src, areas }) => {
-        <ImageMap name={name} src={src} areas={areas} />;
-      })}
+      <ImageMap key={name} name={name} src={src} areas={areas} />
     </div>
   );
+}
+
+export default function Dashboard() {
+  return <Demo name="dashboard"></Demo>;
 }
