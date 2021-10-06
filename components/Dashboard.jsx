ede1fbddfe179d6b722ba520e104b3d47465ca9b
Travis Vachon <travis.vachon@gmail.com>
2021-10-05 19:58:19 -0700

    Extract HeaderWithData for reusability

--- components/Dashboard.jsx
+++ components/Dashboard.jsx
@@ -1,19 +1,15 @@
-import { useMyProfile, useWebId, useLoggedIn, useAuthentication } from 'swrlit'
+import { useWebId } from 'swrlit'
 import WebMonetization from '../components/WebMonetization'
-import Header from '../components/Header'
+import HeaderWithData from '../components/HeaderWithData'
 import { WorkspaceProvider } from '../contexts/WorkspaceContext'
 import Notes from '../components/Notes'
 
 export default function Dashboard() {
   const webId = useWebId()
-  const { profile } = useMyProfile()
-  const loggedIn = useLoggedIn()
-  const {logout} = useAuthentication()
-
   return (
     <>
       <WebMonetization webId={webId} />
-      <Header profile={profile} logout={logout} loggedIn={loggedIn}/>
+      <HeaderWithData />
       <div className="py-6 px-18">
         <WorkspaceProvider webId={webId} slug="default">
           <Notes webId={webId} />
