6ddb2e6480a9e3c4bb0d1c957532a183441a4555
Travis Vachon <travis.vachon@gmail.com>
2021-10-07 12:59:39 -0700

    cleanup now unused components
    they were breaking after the renamed concept hook

--- pages/u/[handle]/[workspace]/[name].jsx
+++ pages/u/[handle]/[workspace]/[name].jsx
@@ -1,18 +1,18 @@
 import { useRouter } from 'next/router'
 import { useWebId } from 'swrlit'
 
-import ConceptPage from "../../../../components/ConceptPage"
+import ConceptPageComponent from "../../../../components/ConceptPage"
 import { handleToWebId } from "../../../../utils/uris"
 import { WorkspaceProvider } from "../../../../contexts/WorkspaceContext"
 
-export default function NotePage(){
+export default function ConceptPage(){
   const router = useRouter()
   const { query: { name, workspace, handle } } = router
   const webId = handleToWebId(handle)
 
   return (
     <WorkspaceProvider webId={webId} slug={workspace}>
-      <ConceptPage webId={webId} workspaceSlug={workspace} slug={name}/>
+      <ConceptPageComponent webId={webId} workspaceSlug={workspace} slug={name}/>
     </WorkspaceProvider>
   )
 }
