3deee2623f51223d2d6e86b405f6089c7118372b
Travis Vachon <travis.vachon@gmail.com>
2021-10-06 19:48:27 -0700

    Fix concept links in sidebar and editor
    Also realized that useCombinedConceptIndex no longer works as it relied on the assumption that a SolidDataset was an RDF dataset, which is no longer true!

--- components/ConnectionsPanel.jsx
+++ components/ConnectionsPanel.jsx
@@ -1,9 +1,27 @@
 import { useState, forwardRef } from 'react'
+import { asUrl } from '@inrupt/solid-client'
+import { useWebId } from 'swrlit'
+import { namedNode } from "@rdfjs/dataset";
+import Link from 'next/link'
+
+import { US } from "../vocab";
+import {
+   getTags, getLinks, tagUrlToTagName,
+  conceptIdFromUri,
+  conceptUrisThatReference,
+ } from '../model/concept'
+import Label from './Label'
+import { notePath, urlSafeIdToConceptName } from "../utils/uris";
+import { useWorkspaceContext } from "../contexts/WorkspaceContext";
+import {
+  useConceptIndex,
+  useCombinedConceptIndex,
+  useConcept,
+  useConceptNames
+} from "../hooks/concepts";
 
 import { Close as CloseIcon } from "./icons"
 import ConnectionsTabs from './ConnectionsTabs'
-import { getTags, getLinks, tagUrlToTagName, conceptUrlToConceptName } from '../model/concept'
-import Label from './Label'
 
 function TagsSection({ concept, tagPrefix }) {
   const tags = getTags(concept)
@@ -16,13 +34,86 @@ function TagsSection({ concept, tagPrefix }) {
   )
 }
 
+function LinkToConcept({ uri, webId, workspaceSlug, ...props }) {
+  const id = conceptIdFromUri(uri);
+  const name = urlSafeIdToConceptName(id);
+  const url = notePath(webId, workspaceSlug, name)
+
+  return (
+    <Link href={url}>
+      <a>
+        <Label>{name}</Label>
+      </a>
+    </Link>
+  );
+}
+
+function LinksTo({ name }) {
+  const webId = useWebId();
+  const { slug: workspaceSlug } = useWorkspaceContext();
+  const { concept } = useConcept(webId, workspaceSlug, name);
+  const conceptUris = concept && getUrlAll(concept, US.refersTo);
+  return (
+    <ul>
+      {conceptUris &&
+        conceptUris.map((uri) => (
+          <li key={uri}>
+            <LinkToConcept uri={uri} />
+          </li>
+        ))}
+    </ul>
+  );
+}
+
+function LinksFrom({ conceptUri }) {
+  const webId = useWebId();
+  const { slug: workspaceSlug } = useWorkspaceContext();
+  const { index } = useCombinedConceptIndex(webId, workspaceSlug);
+  const linkingConcepts = index.match(
+    null,
+    namedNode(US.refersTo),
+    namedNode(conceptUri)
+  );
+  return (
+    <ul>
+      {conceptUrisThatReference(index, conceptUri).map((uri) => (
+        <li key={uri}>
+          <LinkToConcept uri={uri} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function useLinksTo(concept) {
+  const conceptUri = asUrl(concept)
+  const webId = useWebId();
+  const { slug: workspaceSlug } = useWorkspaceContext();
+  // TODO: useCombinedConceptIndex is broken after some changes in the solid library - fix!
+  const { index } = useCombinedConceptIndex(webId, workspaceSlug);
+  const linkingConcepts = index.match(
+    null,
+    namedNode(US.refersTo),
+    namedNode(conceptUri)
+  );
+  return linkingConcepts;
+}
+
 function LinksSection({ concept, conceptPrefix }) {
+  const webId = useWebId();
+  const { slug: workspaceSlug } = useWorkspaceContext();
   const links = getLinks(concept)
+
+  // //TODO
+  // const linksTo = useLinksTo(concept)
+  //console.log("LINKS TO", linksTo)
+
+
   return (
     <div className="p-6">
       {
         links && (links.map(link => (
-          <Label>{conceptUrlToConceptName(link, conceptPrefix)}</Label>
+          <LinkToConcept uri={link} webId={webId} workspaceSlug={workspaceSlug} />
         )))
       }
     </div>
