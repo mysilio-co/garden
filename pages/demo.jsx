2f778513c6ce4b80a84e461ccfad022202db0cee
Ian Davis <jungziege@gmail.com>
2021-10-06 17:13:37 -0700

    Manually layout all links and coordinates

--- pages/demo.jsx
+++ pages/demo.jsx
@@ -3,9 +3,9 @@ import Image from 'next/image'
 import { Loader } from '../components/elements'
 import Nav from '../components/nav'
 
-const Scale = 1 / 3;
-const W = 4050;
-const H = 1950;
+const Scale = 1;
+const W = 1920;
+const H = 924;
 
 export const StoragePrefix =
   "https://ian.myunderstory.com/public/demo/ImageMap/demo/";
@@ -16,33 +16,35 @@ export const DemoPrefix = `${DemoPath}/`;
 export const MountainDashboard = {
   src: `${StoragePrefix}00-mountains-dashboard.jpg`,
   areas: [
-    /*
-Right arrow - links to Mysilio Garden login/signup screen
-Plants - links to Garden dashboard
-    */
-    { // Sun
-      href: `${DemoPrefix}vision`,
-      coords: [1200, 0, 2800, 600],
+    {
+      // Sun
+      href: `${DemoPrefix}sky`,
+      coords: [600, 0, 1400, 300],
     },
-    { // READ ME
-      href: '#',
-      coords: [1900, 800, 2400, 1200],
+    {
+      // READ ME
+      href: "/u/tani.myunderstory.com/default/5LSGKY1KZn",
+      coords: [950, 400, 1200, 600],
     },
-    { // Left Arrow
-      href: `${DemoPrefix}pitch`,
-      coords: [0, 1450, 1000, H],
+    {
+      // Left Arrow
+      href: `${DemoPrefix}path`,
+      coords: [0, 725, 500, H],
     },
-    { // Right Arrow
-      href: 'https://mysilio.garden',
-      coords: [3300, 1450, W, H],
+    {
+      // Right Arrow
+      href: "https://mysilio.garden",
+      coords: [1150,725, W, H],
     },
-    { // Tree
-      href: 'https://mysilio.com',
-      coords: [450, 600, 1500, 1450],
+    {
+      // Tree
+      href: "https://mysilio.com",
+      coords: [225,300, 750, 725],
     },
-    { // Plants
-      href: 'https://mysilio.com',
-      coords: [2600, 900, 3400, 1450],
+    {
+      // Plants
+      href: `${DemoPrefix}forest`,
+      coords: [1300, 450, 1700, 725],
     },
   ],
 };
@@ -50,67 +52,129 @@ Plants - links to Garden dashboard
 export const PathPitch = {
   src: `${StoragePrefix}01-path-pitch.jpg`,
   areas: [
-    /*
-Product vision = https://understory.garden/u/tani.myunderstory.com/default/pFcGoTnHLbR6vXHZdEVW89bZYgmJesMkyK
-Why Now? https://understory.garden/u/tani.myunderstory.com/default/LyQfwzaipmL
-Market Size = https://understory.garden/u/tani.myunderstory.com/default/44VHTqLcTKDP6KQVHYsro
-Team = https://understory.garden/u/tani.myunderstory.com/default/271EUcxT76uHPAgVroyvdM4C
-Funding = https://understory.garden/u/tani.myunderstory.com/default/VcgmBzxsm5ZasszycqkMTenHMP4BQ4
-    */
     {
-      href: DemoPath,
-      coords: [0, 0, W, H],
+      // Funding
+      href: "/u/tani.myunderstory.com/default/VcgmBzxsm5ZasszycqkMTenHMP4BQ4",
+      coords: [700, 50, 1000, 275],
+    },
+    {
+      // Team
+      href: "/u/tani.myunderstory.com/default/271EUcxT76uHPAgVroyvdM4C",
+      coords: [1000, 100, 1400, H],
+    },
+    {
+      // Market Size
+      href: "/u/tani.myunderstory.com/default/44VHTqLcTKDP6KQVHYsro",
+      coords: [0, 0, 1000, H],
+    },
+    {
+      // Why Now?
+      href: "/u/tani.myunderstory.com/default/LyQfwzaipmL",
+      coords: [1000, 0, W, H],
+    },
+    {
+      // Product vision
+      href: "/u/tani.myunderstory.com/default/pFcGoTnHLbR6vXHZdEVW89bZYgmJesMkyK",
+      coords: [450, 0, 900, H],
+    },
+    {
+      // Garden Soil
+      href: "/u/tani.myunderstory.com/default/E2NKVij",
+      coords: [190, 0, 450, H],
     },
   ],
 };
 export const ForestLibrary = {
   src: `${StoragePrefix}02-forest-library.jpg`,
   areas: [
-    /*
-About Solid: https://understory.garden/u/tani.myunderstory.com/default/E2NKVij
-Building the Creator economy: https://understory.garden/u/tani.myunderstory.com/default/21jdcZML2hG1YXioHasgSxujziGBMSWk3FHvCuW
-Worldbuilding: https://understory.garden/u/tani.myunderstory.com/default/6RUdmJM9BtHedkV8wzrzFvQmg7VeZ4RVQBMcdw37
-Metaverse: https://understory.garden/u/tani.myunderstory.com/default/2PmGzU397iuQQ
-The Mysilio Reading Room: https://understory.garden/u/tani.myunderstory.com/default/3AD3JFXpdDPRKP4z8
-Digital Gardens: https://understory.garden/u/tani.myunderstory.com/default/dv4H3AEmdZk6zUNbmuw
-Platform coops: https://understory.garden/u/tani.myunderstory.com/default/iLBJkb92sXiECbA9cvv
-Social knowledge graphs: https://understory.garden/u/tani.myunderstory.com/default/3PHaBYD9kMsF8kdvk1mHKpohYZkpRb3g
-Infinite rooms for thought: https://understory.garden/u/tani.myunderstory.com/default/jRZ5QCkALH1Z2MW1xHsBdKGvbwLVVzubXq
-    */
     {
-      href: DemoPath,
-      coords: [0, 0, W, H],
+      //  Butterfly // Infinite rooms for thought
+      href: "/u/tani.myunderstory.com/default/jRZ5QCkALH1Z2MW1xHsBdKGvbwLVVzubXq",
+      coords: [200, 370, 380, 600],
+    },
+    {
+      // Normal Bee // Building the Creator economy
+      href: " /u/tani.myunderstory.com/default/21jdcZML2hG1YXioHasgSxujziGBMSWk3FHvCuW",
+      coords: [480, 0, 780, 500],
+    },
+    {
+      // Plants // Digital Gardens
+      href: "/u/tani.myunderstory.com/default/dv4H3AEmdZk6zUNbmuw",
+      coords: [840, 0, 1500, 600],
+    },
+    {
+      // Cartoon Bee // Metaverse
+      href: "/u/tani.myunderstory.com/default/2PmGzU397iuQQ",
+      coords: [1600, 140, 1750, 320],
+    },
+    {
+      // Two Shroom Snail // Platform Coops
+      href: "/u/tani.myunderstory.com/default/iLBJkb92sXiECbA9cvv",
+      coords: [0, 650, 420, H],
+    },
+    {
+      // Single Shroom // Mysilio Reading Room
+      href: "/u/tani.myunderstory.com/default/3AD3JFXpdDPRKP4z8",
+      coords: [700, 610, 970, H],
+    },
+    {
+      // Triple Shroom // Social Knowledge Graphs
+      href: "/u/tani.myunderstory.com/default/3PHaBYD9kMsF8kdvk1mHKpohYZkpRb3g",
+      coords: [1140, 610, 1390, H],
+    },
+    {
+      // Gnome // Worldbuilding
+      href: "/u/tani.myunderstory.com/default/6RUdmJM9BtHedkV8wzrzFvQmg7VeZ4RVQBMcdw37",
+      coords: [1610, 710, W, H],
     },
   ],
 };
 
 export const SkyVision = {
-  src:`${StoragePrefix}03-sky-vision.jpg`,
+  src: `${StoragePrefix}03-sky-vision.jpg`,
   areas: [
-    /*
-Sun = Vision/Manifesto = https://understory.garden/u/tani.myunderstory.com/default/21jdcZML2hG1YXioHasxihdiw6feEENXejepjdt
-Birds = Ian/Tani/Travis
-Ian - https://understory.garden/u/ian.myunderstory.com/default/HHkbwnJkxhr
-Travis - https://understory.garden/u/travis.myunderstory.com/default/4bZUgk5Cs7UuP9Eg5QeT5ZrAwAYgQNLSyZ5M
-  ??????? this currently goes to a totally different note, ask Travis what's up with that and get correct link
-Tani - https://understory.garden/u/tani.myunderstory.com/default/3yYHoe
-Could also link "understory family" note instead of individuals on the birds (if we want to be able to include advisors, etc): https://understory.garden/u/tani.myunderstory.com/default/271EUcxT76uHPAgVroyvdM4C
-Cloud = about the Mysilio brand - https://understory.garden/u/tani.myunderstory.com/default/FW445yWvwojgTfQJBjQUFu
-Cloud = our Art 'n D approach: https://understory.garden/u/tani.myunderstory.com/default/BzfF7kX
-Read me note (to be written): https://understory.garden/u/tani.myunderstory.com/default/5LSGKY1KZn
-    */
     {
-      href: DemoPath,
-      coords: [0, 0, W, H],
+      // Left Cloud  // Mysilio Brand
+      href: "/u/tani.myunderstory.com/default/FW445yWvwojgTfQJBjQUFu",
+      coords: [0, 0, 530, 650],
+    },
+    {
+      // Sun // Vision
+      href: "/u/tani.myunderstory.com/default/21jdcZML2hG1YXioHasxihdiw6feEENXejepjdt",
+      coords: [530, 0, 980, 500],
+    },
+    {
+      // Blue Bird // Tani
+      href: "/u/tani.myunderstory.com/default/3yYHoe",
+      coords: [980, 250, 1280, 550],
+    },
+    {
+      // Orange Bird // Ian
+      href: "/u/ian.myunderstory.com/default/HHkbwnJkxhr",
+      coords: [1280, 520, 1620, H],
+    },
+    {
+      // Yellow Bird // Travis
+      href: "/u/travis.myunderstory.com/default/4bZUgk5Cs7UuP9Eg5QeT5ZrAwAYgQNLSyZ5M",
+      coords: [820, 550, 1150, H],
+    },
+    {
+      // Right Cloud  // Art n' D
+      href: "/u/tani.myunderstory.com/default/BzfF7kX",
+      coords: [1300, 0, W, 550],
     },
   ],
- }
+};
 
 export const DemoImageMaps = {
   dashboard: MountainDashboard,
   pitch: PathPitch,
   library: ForestLibrary,
   vision: SkyVision,
+  mountain: MountainDashboard,
+  path: PathPitch,
+  forest: ForestLibrary,
+  sky: SkyVision,
 }
 
 export function AreaLink({ coords, href }) {
