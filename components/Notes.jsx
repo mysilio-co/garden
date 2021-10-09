b9a5dbd9719a2d80ae91fe5b86bbedb7e1354ed2
Travis Vachon <travis.vachon@gmail.com>
2021-10-08 18:54:58 -0700

    Get headers looking different in different contexts
    Make dashboard header pretty

--- components/Notes.jsx
+++ components/Notes.jsx
@@ -1,4 +1,5 @@
 import { asUrl } from "@inrupt/solid-client";
+import { useWebId } from "swrlit";
 
 import { useWorkspaceContext } from "../contexts/WorkspaceContext";
 
@@ -14,13 +15,14 @@ export function NotesFromConcepts({ concepts, webId, workspaceSlug }) {
       {concepts &&
         concepts.map((concept) => (
           <NoteCard key={asUrl(concept)} concept={concept}
-                    webId={webId} workspaceSlug={workspaceSlug}/>
+            webId={webId} workspaceSlug={workspaceSlug} />
         ))}
     </ul>
   );
 }
 
 export default function Notes({ }) {
+  const myWebId = useWebId()
   const { slug: workspaceSlug, webId } = useWorkspaceContext()
   const { concepts } = useConcepts(webId, workspaceSlug);
 
@@ -31,7 +33,12 @@ export default function Notes({ }) {
       ) : (
         <div>
           <h2 className="text-2xl mb-2">
-            Add something to your garden using the Create menu above ^^
+            {(myWebId === webId) ? (
+              "You have no notes. Add something to your garden using the New button in the header above."
+            ) : (
+              `No notes.`
+            )}
+
           </h2>
         </div>
       )) : (
--- stories/Header.stories.jsx
+++ stories/Header.stories.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Header from '../components/Header';
 import avatarImg from './assets/avatar.jpeg'
+import authorAvatarImg from './assets/authorAvatar.jpeg'
 import { createProfileFor } from '../model/profile'
 
 export default {
@@ -17,13 +18,19 @@ const profile = createProfileFor(
   new URL(avatarImg, window.location).toString()
 );
 
+const authorProfile = createProfileFor(
+  "https://example.com/webids/author",
+  new URL(authorAvatarImg, window.location).toString()
+);
+
 const Template = (args) => <Header {...args} />
 
 export const DashboardHeader = Template.bind({});
 DashboardHeader.args = {
   label: 'Header/Dashboard',
   profile,
-  loggedIn: true
+  loggedIn: true,
+  type: 'dashboard'
 };
 
 export const DashboardHeaderDrawerOpen = Template.bind({});
@@ -31,6 +38,16 @@ DashboardHeaderDrawerOpen.args = {
   label: 'Header/DashboardDrawerOpen',
   profile,
   drawerOpen: true,
-  loggedIn: true
+  loggedIn: true,
+  type: 'dashboard'
+};
+
+export const ProfileHeader = Template.bind({});
+ProfileHeader.args = {
+  label: 'Header/Profile'
+  ,
+  profile,
+  loggedIn: true,
+  type: 'profile'
 };
 
