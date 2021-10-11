ea7aa72f1c9ba77a57fd11751628eeed0d24829f
Ian Davis <jungziege@gmail.com>
2021-10-11 15:38:42 -0700

    Update File Upload style

--- components/modals/NewUpload.jsx
+++ components/modals/NewUpload.jsx
@@ -1,43 +1,78 @@
 import { useState, Fragment } from 'react'
 import { Formik, Field, Form } from "formik";
-import * as Yup from "yup";
 import { Transition, Dialog } from '@headlessui/react';
+import { Close as CloseIcon, TickCircle } from '../icons'
+import * as Yup from "yup";
 
 export function NewUpload ({ onClose }) {
   return (
-    <Formik
-      initialValues={{ file: null }}
-      onSubmit={async (values) => {
-        console.alert(
-          JSON.stringify(
-            {
-              fileName: values.file.name,
-              type: values.file.type,
-              size: `${values.file.size} bytes`,
-            },
-            null,
-            2
-          )
-        );
-      }}
-      validationSchema={Yup.object().shape({
-        file: Yup.mixed().required(),
-      })}
-    >
-      <Form>
-        <div className="form-group">
-          <label for="file">File upload</label>
-          <Field id="file" name="file" type="file" className="form-control" />
+    <div className="mx-auto rounded-lg overflow-hidden bg-white flex flex-col items-stretch">
+      <div className="flex flex-row justify-between self-stretch h-18 p-6 bg-my-green">
+        <div className="flex flex-row justify-start items-start gap-4">
+          <h2 className="text-white font-bold text-xl">New Upload</h2>
         </div>
-        <button type="submit" className="btn">
-          Submit
-        </button>
-        <button type="button" className="btn cancel"
-          onClick={onClose}>
-          Cancel
-        </button>
-      </Form>
-    </Formik>
+        <CloseIcon
+          className="text-white h-6 w-6 flex-grow-0 cursor-pointer"
+          onClick={onClose}
+        />
+      </div>
+      <div className="divide-1 divide-gray-100">
+        <Formik
+          initialValues={{ file: null }}
+          onSubmit={async (values) => {
+            console.alert(
+              JSON.stringify(
+                {
+                  fileName: values.file.name,
+                  type: values.file.type,
+                  size: `${values.file.size} bytes`,
+                },
+                null,
+                2
+              )
+            );
+          }}
+          validationSchema={Yup.object().shape({
+            file: Yup.mixed().required(),
+          })}
+        >
+          <Form>
+            <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start px-6 py-5">
+              <label
+                htmlFor="file"
+                className="text-sm font-medium text-gray-900"
+              >
+                Upload a file or image
+              </label>
+              <div className="mt-1 sm:mt-0 sm:col-span-2 flex flex-col">
+                <Field
+                  id="file"
+                  name="file"
+                  type="file"
+                  className="ipt"
+                />
+              </div>
+            </div>
+            <div className="h-20 bg-gray-50 flex flex-row justify-end items-center px-6">
+              <button
+                type="button"
+                className="btn-md btn-filled btn-square h-10 mr-1"
+                onClick={onClose}
+              >
+                Cancel
+              </button>
+              <button
+                type="submit"
+                className="btn-md btn-filled btn-square h-10 ring-my-green text-my-green flex flex-row justify-center items-center"
+              >
+                Create
+                <TickCircle className="ml-1 text-my-green h-4 w-4" />
+              </button>
+            </div>
+          </Form>
+        </Formik>
+      </div>
+    </div>
   );
 }
 
