eaa4969e90f5f21ee39aeca2634aaa87b309f688
Ian Davis <jungziege@gmail.com>
2021-10-11 15:29:31 -0700

    Update style on NewBookmarkModal

--- components/modals/NewBookmark.jsx
+++ components/modals/NewBookmark.jsx
@@ -1,30 +1,60 @@
 import { useState, Fragment } from 'react'
 import { Transition, Dialog } from '@headlessui/react';
 import { Formik, Field, Form } from "formik";
+import { Close as CloseIcon, TickCircle } from '../icons'
 
 export function NewBookmark({ onClose }) {
   return (
-    <Formik
-      initialValues={{ url: '' }}
-      onSubmit={async (values) => {
-        await new Promise((resolve) => setTimeout(resolve, 500));
-        alert(JSON.stringify(values, null, 2));
-      }}
-    >
-      <Form>
-        <Field name="url" type="url" />
-        <button type="submit" className="btn">
-          Submit
-        </button>
-        <button
-          type="button"
-          className="btn cancel"
+    <div className="mx-auto rounded-lg overflow-hidden bg-white flex flex-col items-stretch">
+      <div className="flex flex-row justify-between self-stretch h-18 p-6 bg-my-green">
+        <div className="flex flex-row justify-start items-start gap-4">
+          <h2 className="text-white font-bold text-xl">New Bookmark</h2>
+        </div>
+        <CloseIcon
+          className="text-white h-6 w-6 flex-grow-0 cursor-pointer"
           onClick={onClose}
+        />
+      </div>
+      <div className="divide-1 divide-gray-100">
+        <Formik
+          initialValues={{ url: '' }}
+          onSubmit={async (values) => {
+            await new Promise((resolve) => setTimeout(resolve, 500));
+            alert(JSON.stringify(values, null, 2));
+          }}
         >
-          Cancel
-        </button>
-      </Form>
-    </Formik>
+          <Form>
+            <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start px-6 py-5">
+              <label
+                htmlFor="name"
+                className="text-sm font-medium text-gray-900"
+              >
+                URL
+              </label>
+              <div className="mt-1 sm:mt-0 sm:col-span-2 flex flex-col">
+                <Field type="url" name="url" id="url" className="ipt" />
+              </div>
+            </div>
+            <div className="h-20 bg-gray-50 flex flex-row justify-end items-center px-6">
+              <button
+                type="button"
+                className="btn-md btn-filled btn-square h-10 mr-1"
+                onClick={onClose}
+              >
+                Cancel
+              </button>
+              <button
+                type="submit"
+                className="btn-md btn-filled btn-square h-10 ring-my-green text-my-green flex flex-row justify-center items-center"
+              >
+                Create
+                <TickCircle className="ml-1 text-my-green h-4 w-4" />
+              </button>
+            </div>
+          </Form>
+        </Formik>
+      </div>
+    </div>
   );
 }
 
