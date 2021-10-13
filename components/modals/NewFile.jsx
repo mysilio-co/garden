ea19b41907093ad081f7bc1dce9f8644d0b8d49e
Ian Davis <jungziege@gmail.com>
2021-10-12 21:50:28 -0700

    Use new Modal component for modals/*

--- components/modals/NewFile.jsx
+++ components/modals/NewFile.jsx
@@ -8,6 +8,7 @@ import { useWebId } from 'swrlit';
 import { addFileToIndex } from '../../model';
 import { uploadFromFile } from '../../components/ImageUploader';
 import { useFileContainerUri } from '../../hooks/uris';
+import Modal from '../Modal';
 
 export function NewFile({ onClose }) {
   const webId = useWebId();
@@ -78,39 +79,8 @@ export function NewFile({ onClose }) {
 
 export default function NewFileModal({ open, onClose }) {
   return (
-    <Transition.Root show={open} as={Fragment}>
-      <Dialog
-        as="div"
-        onClose={onClose}
-        className="fixed z-10 inset-0 overflow-y-auto"
-      >
-        <div className="flex items-end justify-center min-h-screen px-4 pb-20 text-center sm:block sm:p-0">
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-20 hams transition-opacity backdrop-filter backdrop-blur-lg" />
-          </Transition.Child>
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            enterTo="opacity-100 translate-y-0 sm:scale-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-          >
-            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-full sm:w-5/6">
-              <NewFile onClose={onClose} />
-            </div>
-          </Transition.Child>
-        </div>
-      </Dialog>
-    </Transition.Root>
+    <Modal open={open} onClose={onClose}>
+      <NewFile onClose={onClose} />
+    </Modal>
   );
 }
--- components/modals/NewNote.jsx
+++ components/modals/NewNote.jsx
@@ -17,6 +17,7 @@ import { useCurrentWorkspace } from "../../hooks/app";
 import { useConcept, useConceptNames } from "../../hooks/concepts";
 import NoteEditor from "../NoteEditor"
 import { EmptySlateJSON } from "../../utils/slate";
+import Modal from '../Modal';
 
 
 export const NewNote = ({ onClose, isPublic = false }) => {
@@ -135,39 +136,8 @@ export const NewNote = ({ onClose, isPublic = false }) => {
 
 export default function NewNoteModal({ isPublic = false, conceptNames, open, onClose }) {
   return (
-    <Transition.Root show={open} as={Fragment}>
-      <Dialog
-        as="div"
-        onClose={onClose}
-        className="fixed z-10 inset-0 overflow-y-auto"
-      >
-        <div className="flex items-end justify-center min-h-screen px-4 pb-20 text-center sm:block sm:p-0">
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-20 hams transition-opacity backdrop-filter backdrop-blur-lg" />
-          </Transition.Child>
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            enterTo="opacity-100 translate-y-0 sm:scale-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-          >
-            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-full sm:w-5/6">
-              <NewNote onClose={onClose} conceptNames={conceptNames} />
-            </div>
-          </Transition.Child>
-        </div>
-      </Dialog>
-    </Transition.Root>
+    <Modal open={open} onClose={onClose}>
+      <NewNote onClose={onClose} conceptNames={conceptNames} />
+    </Modal>
   );
 }
