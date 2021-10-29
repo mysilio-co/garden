2ab4c9bf43a3ab3fde6adb4ed58c6e2c142f9628
Ian Davis <jungziege@gmail.com>
2021-10-29 10:50:24 -0700

    prefix vercel env with next public

--- model/flags.js
+++ model/flags.js
@@ -1,6 +1,6 @@
 import { collapseTextChangeRangesAcrossMultipleVersions } from "typescript"
 
-const VercelEnv = process.env.VERCEL_ENV || ''
+const VercelEnv = process.env.NEXT_PUBLIC_VERCEL_ENV || '';
 
 export function isPreviewEnv() {
   console.log(VercelEnv);
