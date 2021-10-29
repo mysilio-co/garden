d41d0d557b83fe5632a61fa86326070af5a72cbc
Ian Davis <jungziege@gmail.com>
2021-10-29 11:06:11 -0700

    Env flag works, remove console.log

--- model/flags.js
+++ model/flags.js
@@ -3,6 +3,5 @@ import { collapseTextChangeRangesAcrossMultipleVersions } from "typescript"
 const VercelEnv = process.env.NEXT_PUBLIC_VERCEL_ENV || '';
 
 export function isPreviewEnv() {
-  console.log(VercelEnv);
   return VercelEnv !== 'production';
 }
