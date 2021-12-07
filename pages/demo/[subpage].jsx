afe994a8e2cbb586d6d04ad9e553f48464a8e2cd
Ian Davis <jungziege@gmail.com>
2021-12-07 13:02:35 -0800

    Remove image map demo

--- pages/demo/[subpage].jsx
+++
@@ -1,8 +0,0 @@
-import { useRouter } from "next/router";
-import { Demo } from "../demo";
-
-export default function DemoSubpage() {
-  const router = useRouter();
-  const { subpage } = router.query;
-  return <Demo name={subpage}></Demo>;
-}
