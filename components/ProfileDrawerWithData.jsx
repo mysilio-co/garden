ca19a0937a9b59a38c0a56651b12aa6fb566d235
Travis Vachon <travis.vachon@gmail.com>
2022-01-31 14:57:36 -0800

    redirect to / on profile drawer logout

--- components/ProfileDrawerWithData.jsx
+++ components/ProfileDrawerWithData.jsx
@@ -1,7 +1,8 @@
+import { useCallback } from 'react'
 import { useProfile, useLoggedIn, useAuthentication } from 'swrlit'
+import { useRouter } from 'next/router'
 
 import { useConceptNames } from '../hooks/concepts'
-
 import ProfileDrawer from './ProfileDrawer'
 
 export default function ProfileDrawerWithData({ webId, ...props }) {
@@ -9,10 +10,15 @@ export default function ProfileDrawerWithData({ webId, ...props }) {
   const { logout } = useAuthentication()
   const conceptNames = useConceptNames()
   const { profile, save: saveProfile } = useProfile(webId)
+  const router = useRouter()
+  const logoutAndRedirect = useCallback(() => {
+    logout()
+    router.push("/")
+  }, [router, logout])
   return (
     <ProfileDrawer
       loggedIn={loggedIn}
-      logout={logout}
+      logout={logoutAndRedirect}
       conceptNames={conceptNames}
       profile={profile}
       saveProfile={saveProfile}
