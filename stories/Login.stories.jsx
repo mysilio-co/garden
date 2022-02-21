77168c3b6898ceb3f9d4736f4492e96fd208eb02
Travis Vachon <travis.vachon@gmail.com>
2022-02-21 14:12:55 -0800

    add storybook stories for new login stuff

---
+++ stories/Login.stories.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Login from '../components/Login';
+import avatarImg from './assets/avatar.jpeg';
+
+export default {
+  title: 'Components/Login',
+  component: Login,
+  parameters: {
+    layout: 'fullscreen'
+  },
+  argTypes: {
+  }
+};
+
+const Template = (args) => <Login {...args} />
+
+export const LoginLanding = Template.bind({});
+LoginLanding.args = {
+  label: 'Login/Landing',
+  username: "dylanfrazo",
+};
