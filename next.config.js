37f7bfde420754af07e43fed363d73d82d4c05be
Travis Vachon <travis.vachon@gmail.com>
2022-03-24 19:00:15 -0700

    re-enabled minification
    the issues with image-blob-reduce seem to have gone away, yay

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ module.exports = {
     // IMPORTANT: we need to disable minimization for now because image-blob-reduce
     // has problems with terser minimization:
     // https://github.com/nodeca/image-blob-reduce
-    config.optimization.minimize = false;
+    //config.optimization.minimize = false;
     // Important: return the modified config
     return config;
   },
