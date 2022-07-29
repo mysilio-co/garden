841040e2868b8eb79bf8f99d771c800518b673fd
Travis Vachon <travis.vachon@gmail.com>
2022-07-29 11:40:24 -0700

    remove unused page

--- pages/foo.jsx
+++
@@ -1,7 +0,0 @@
-import { getUrl } from '@inrupt/solid-client/thing/get'
-import { useThing } from 'swrlit/hooks/things'
-export default function Foo() {
-  const {data} = useThing("https://travis.mysilio.me/public/public.ttl")
-  const bar = getUrl(null)
-  return <div>foo {bar} {data}</div>
-}
