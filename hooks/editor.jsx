3cea4df8583f1990015d514396ac2ab18f780288
Travis Vachon <travis.vachon@gmail.com>
2022-08-17 12:25:33 -0700

    fix debounce related save issue
    if you made a change and then switched notes before the change saved it would overwrite the note you switched to - bad!
    
    using useDebounceCallback fixes this nicely - we're still dropping the last save in the case above, but this prevents overwriting data

--- hooks/editor.jsx
+++ hooks/editor.jsx
@@ -1,49 +1,31 @@
 import { useRef, useEffect, useCallback, useState, useMemo } from 'react'
 import { Editor, Transforms, Range } from 'slate'
-import { useDebounce } from "use-debounce";
+import { useDebouncedCallback } from "use-debounce";
 
 import { useConceptNamesMatching } from '../hooks/concepts'
 import { insertConcept } from '../utils/editor'
 
-export function useAutosave(note, save) {
-  const [updatedValue, setUpdatedValue] = useState()
-  const [debouncedUpdatedValue] = useDebounce(updatedValue, 1500);
+export function useAutosave(item, save) {
 
-  // make sure
+  const debounced = useDebouncedCallback(save, 1500)
+
+  // cancel debounce whenever item changes to avoid overwriting 
   useEffect(function () {
-    if (!note) {
-      setUpdatedValue(null)
-    }
-  }, [note])
+    debounced.cancel()
+  }, [item])
 
-
-  // use a ref here to avoid needing to add more dependencies to the useEffect.
-  // we'd like to take advantage of useEffect only running when debouncedUpdatedValue
-  // changes, so we're using a ref to ensure the effect always has access to the latest
-  // save function without adding the function to the dependencies
-  const saveRef = useRef()
-  saveRef.current = save;
-  useEffect(function saveIfValueExists() {
-    if (debouncedUpdatedValue) {
-      saveRef.current(debouncedUpdatedValue)
-    }
-  },
-    // !!! do not add more dependencies - this should only re-run when debouncedUpdatedValue changes !!!
-    [debouncedUpdatedValue]
-  )
-
-  return { onChange: setUpdatedValue }
+  return { onChange: debounced }
 }
 
-function searchForOpenConcepts(editor){
+function searchForOpenConcepts(editor) {
   const { selection } = editor
-  if (selection && Range.isCollapsed(selection)){
+  if (selection && Range.isCollapsed(selection)) {
     const [start] = Range.edges(selection)
-    const before = {...start, offset: 0}
+    const before = { ...start, offset: 0 }
     const beforeRange = before && Editor.range(editor, before, start)
     const beforeText = beforeRange && Editor.string(editor, beforeRange)
     const beforeMatch = beforeText && beforeText.match(/\[\[([^\]]*)$/)
-    const conceptStart = before && beforeMatch && {path: before.path, offset: beforeMatch && beforeMatch.index}
+    const conceptStart = before && beforeMatch && { path: before.path, offset: beforeMatch && beforeMatch.index }
     const conceptRange = conceptStart && Editor.range(editor, conceptStart, start)
     const conceptText = conceptRange && Editor.string(editor, conceptRange)
     const conceptMatch = conceptText && conceptText.match(/\[\[([^\]]*)$/)
@@ -52,20 +34,20 @@ function searchForOpenConcepts(editor){
     const afterText = Editor.string(editor, afterRange)
     const afterMatch = afterText.match(/^(\s|$)/)
     if (conceptMatch && afterMatch) {
-      return {target: conceptRange, search: conceptMatch[1]}
+      return { target: conceptRange, search: conceptMatch[1] }
     }
   }
 }
 
-export function useConceptAutocomplete(editor){
+export function useConceptAutocomplete(editor) {
   const [target, setTarget] = useState()
   const [search, setSearch] = useState()
   const [selectionIndex, setPopoverSelectionIndex] = useState()
   const names = useConceptNamesMatching(search)
 
-  const onChange = useCallback(function onChange(){
+  const onChange = useCallback(function onChange() {
     const openConcepts = searchForOpenConcepts(editor)
-    if (openConcepts){
+    if (openConcepts) {
       setTarget(openConcepts.target)
       setSearch(openConcepts.search)
       setPopoverSelectionIndex(0)
@@ -79,27 +61,27 @@ export function useConceptAutocomplete(editor){
     event => {
       if (target && names) {
         switch (event.key) {
-        case 'ArrowDown':
-          event.preventDefault()
-          const prevIndex = selectionIndex >= names.length - 1 ? 0 : selectionIndex + 1
-          setPopoverSelectionIndex(prevIndex)
-          break
-        case 'ArrowUp':
-          event.preventDefault()
-          const nextIndex = selectionIndex <= 0 ? names.length - 1 : selectionIndex - 1
-          setPopoverSelectionIndex(nextIndex)
-          break
-        case 'Tab':
-        case 'Enter':
-          event.preventDefault()
-          Transforms.select(editor, target)
-          insertConcept(editor, names[selectionIndex])
-          setTarget(null)
-          break
-        case 'Escape':
-          event.preventDefault()
-          setTarget(null)
-          break
+          case 'ArrowDown':
+            event.preventDefault()
+            const prevIndex = selectionIndex >= names.length - 1 ? 0 : selectionIndex + 1
+            setPopoverSelectionIndex(prevIndex)
+            break
+          case 'ArrowUp':
+            event.preventDefault()
+            const nextIndex = selectionIndex <= 0 ? names.length - 1 : selectionIndex - 1
+            setPopoverSelectionIndex(nextIndex)
+            break
+          case 'Tab':
+          case 'Enter':
+            event.preventDefault()
+            Transforms.select(editor, target)
+            insertConcept(editor, names[selectionIndex])
+            setTarget(null)
+            break
+          case 'Escape':
+            event.preventDefault()
+            setTarget(null)
+            break
         }
       }
     },
@@ -107,5 +89,5 @@ export function useConceptAutocomplete(editor){
   )
 
 
-  return {onChange, onKeyDown, names, target, selectionIndex}
+  return { onChange, onKeyDown, names, target, selectionIndex }
 }
