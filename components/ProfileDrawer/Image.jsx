498857b378bb190ff05eeaa6ab610cb65d7b481a
Travis Vachon <travis.vachon@gmail.com>
2022-08-16 18:41:45 -0700

    support setting profile image
    - show dummy avatar for users who don't have any image: this gets the onhover edit button working again
    - set proper permissions on image after upload
    - fix a bug with the image uploader cancel button

--- components/ProfileDrawer/Image.jsx
+++ components/ProfileDrawer/Image.jsx
@@ -3,6 +3,8 @@ import { useWebId } from 'swrlit/contexts/authentication';
 import { getUrl } from '@inrupt/solid-client/thing/get';
 import { setUrl } from '@inrupt/solid-client/thing/set';
 import { FOAF } from '@inrupt/vocab-common-rdf';
+import { setPublicAccess } from 'garden-kit/acl'
+import { useAuthentication } from 'swrlit/contexts/authentication'
 import { useImageUploadUri } from '../../hooks/uris';
 import { EditIcon } from '../icons';
 import { ImageUploadAndEditor } from '../ImageUploader';
@@ -14,8 +16,11 @@ export default function ProfileImage({ profile, saveProfile, ...props }) {
   const [editingProfileImage, setEditingProfileImage] = useState(false);
   const webId = useWebId();
   const imageUploadContainer = useImageUploadUri(webId);
-  function saveProfileImage(newProfileImageUri) {
-    saveProfile(setUrl(profile, FOAF.img, newProfileImageUri));
+  const { fetch } = useAuthentication()
+
+  async function saveProfileImage(newProfileImageUri) {
+    await setPublicAccess(newProfileImageUri, { read: true, append: false, write: false, control: false }, { fetch })
+    await saveProfile(setUrl(profile, FOAF.img, newProfileImageUri));
     setEditingProfileImage(false);
   }
   function onEdit() {
@@ -23,15 +28,19 @@ export default function ProfileImage({ profile, saveProfile, ...props }) {
   }
   return (
     <div {...props}>
-      <div className="relative cursor-pointer group" onClick={onEdit}>
-        {profileImage && <Avatar className="rounded-full h-32 w-32 object-cover mr-12" src={profileImage} />}
+      <div className="relative cursor-pointer group text-white" onClick={onEdit}>
+        <Avatar className="rounded-full h-32 w-32 object-cover" src={profileImage} />
         <div className="absolute inset-0 justify-center items-center hidden group-hover:flex group-hover:bg-white opacity-50">
           <EditIcon className="text-gray-900 w-10 h-10" />
         </div>
         <Modal open={editingProfileImage} onClose={() => { setEditingProfileImage(false) }}>
           <ImageUploadAndEditor onSave={saveProfileImage}
             imageUploadContainerUri={imageUploadContainer}
-            onClose={() => setEditingProfileImage(false)} />
+            onClose={(e) => {
+              e.preventDefault()
+              e.stopPropagation()
+              setEditingProfileImage(false)
+            }} />
         </Modal>
       </div>
     </div>
