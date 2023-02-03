c1f6d1946fe4348d21b91afcfa6ed7c7c70f936f
Ian Davis <jungziege@gmail.com>
2023-02-03 13:28:52 +0000

    Add Webhook Settings management UI

--- components/ProfileDrawer/Webhooks.jsx
+++ components/ProfileDrawer/Webhooks.jsx
@@ -1,14 +1,16 @@
 import { useEffect, useState } from 'react';
-import { getUrl } from '@inrupt/solid-client';
+import { asUrl, getUrl } from '@inrupt/solid-client';
 import {
-  getContainer,
+  gardenMetadataInSpacePrefs,
   getSpace,
+  getTitle,
   HomeSpaceSlug,
   MY,
   useSpaces,
   useWebhooks,
 } from 'garden-kit';
 import { useWebId } from 'swrlit';
+import { useMemo } from 'react';
 
 export default function Webhooks({ profile, saveProfile, ...props }) {
   const { webhooks, addWebhookSubscription, unsubscribeFromWebhook } =
@@ -16,41 +18,59 @@ export default function Webhooks({ profile, saveProfile, ...props }) {
 
   const webId = useWebId();
   const { spaces } = useSpaces(webId);
-  // for simplicity, only Gnomes for the Home space can be setup here.
-  // Community Gnomes should be configured manually.
+
+  // For simplicity, only setup Gnomes for the Gardens in the user's Home space.
+  // Community Gnomes should be configured by logging in as the Community Pod.
   const home = spaces && getSpace(spaces, HomeSpaceSlug);
-  const homeContainer = getContainer(home);
+  const gardens = gardenMetadataInSpacePrefs(home, spaces);
 
-  const homeSpaceWebhooks = webhooks.filter(
-    (webhook) => getUrl(webhook, MY.Garden.subscribedTo) == homeContainer
-  );
-  console.log(webhooks);
-
-  useEffect(() => {
-    if (homeSpaceWebhooks.length > 0) {
-      setEnabled(true);
-    } else {
-      setEnabled(false);
-    }
-  }, [homeSpaceWebhooks]);
-
-  const [enabled, setEnabled] = useState(false);
-
-  async function onChange() {
-    if (!enabled) {
-      await addWebhookSubscription(
-        homeContainer,
-        `https://${window.location.host}/api/webhooks`
-      );
-    } else {
-      const homeSpaceWebhooks = webhooks.filter(
-        (webhook) => getUrl(webhook, MY.Garden.subscribedTo) == homeContainer
-      );
-      for (const webhook of homeSpaceWebhooks) {
-        await unsubscribeFromWebhook(webhook);
-      }
+  async function enableAll() {
+    for (const garden of gardens) {
+      const gardenUrl = asUrl(garden);
+      await enable(gardenUrl);
     }
   }
+  async function disableAll() {
+    for (const gardenUrl of enabled) {
+      await disable(gardenUrl);
+    }
+  }
+
+  async function enable(gardenUrl) {
+    await addWebhookSubscription(
+      gardenUrl,
+      `https://${window.location.host}/api/webhooks`
+    );
+  }
+  async function disable(gardenUrl) {
+    await unsubscribeFromWebhook(getWebhook(gardenUrl));
+  }
+  async function toggle(gardenUrl) {
+    if (getWebhook(gardenUrl)) disable(gardenUrl);
+    else enable(gardenUrl);
+  }
+  async function toggleAll() {
+    if (enabled.size > 0) disableAll();
+    else enableAll();
+  }
+
+  function getWebhook(gardenUrl) {
+    return webhooks.find(
+      (webhook) => getUrl(webhook, MY.Garden.subscribedTo) == gardenUrl
+    );
+  }
+
+  const enabled = useMemo(() => {
+    const enabled = new Set();
+    for (const garden of gardens) {
+      const gardenUrl = asUrl(garden);
+      if (getWebhook(gardenUrl)) {
+        enabled.add(gardenUrl);
+      }
+    }
+    console.log(enabled);
+    return enabled;
+  }, [webhooks, gardens]);
 
   return (
     <div {...props}>
@@ -59,21 +79,47 @@ export default function Webhooks({ profile, saveProfile, ...props }) {
           <input
             name="gnomes"
             type="checkbox"
-            value={enabled}
-            onChange={onChange}
+            checked={enabled.size > 0}
+            onChange={toggleAll}
             className="h-4 w-4 rounded border-gray-300 text-purple-500 focus:ring-purple-400"
           />
         </div>
         <div className="ml-3 text-sm">
           <label className="font-medium text-purple-300">
-            Enable Garden Gnomes {enabled}
+            Enable Garden Gnomes
           </label>
           <p className="text-gray-400 text-xs">
-            Garden Gnomes help keep your Gardens clean, and are necessay for
+            Garden Gnomes help keep your Gardens clean, and are required for
             advanced features like full-text search.
           </p>
         </div>
       </div>
+      {enabled.size > 0 &&
+        gardens &&
+        gardens.map((garden) => {
+          return (
+            <div key={asUrl(garden)} className="ml-6">
+              <div className="relative flex items-start">
+                <div className="flex h-5 items-center">
+                  <input
+                    name={getTitle(garden)}
+                    type="checkbox"
+                    checked={enabled.has(asUrl(garden))}
+                    onChange={() => {
+                      toggle(asUrl(garden));
+                    }}
+                    className="h-4 w-4 rounded border-gray-300 text-purple-500 focus:ring-purple-400"
+                  />
+                </div>
+                <div className="ml-3 text-sm">
+                  <label className="font-small text-purple-300">
+                    {getTitle(garden)}
+                  </label>
+                </div>
+              </div>
+            </div>
+          );
+        })}
     </div>
   );
 }
