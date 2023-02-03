0b8d1e7cf1caf671c5e93fe2d7e955dc3f4eb671
Ian Davis <jungziege@gmail.com>
2023-02-03 15:00:36 +0000

    Simple webhook handler that logs received webhook

---
+++ pages/api/webhooks.js
@@ -0,0 +1,19 @@
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    console.log('Webhook request');
+    if (
+      (await verifyAuthIssuer(req.headers.authorization)) === req.body.origin
+    ) {
+      console.log('Webhook valid');
+      console.log(req.body);
+    } else {
+      console.log('This issuer is invalid');
+    }
+
+    res.status(200);
+  } else {
+    return res
+      .status(405)
+      .json({ message: 'The webhooks handler only accepts POST requests' });
+  }
+}
