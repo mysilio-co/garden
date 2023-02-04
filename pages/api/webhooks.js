4c24b141ecada1e1a17d1f8bc663e3a15703d64c
Ian Davis <jungziege@gmail.com>
2023-02-04 16:37:52 +0000

    Actually return status codes from webhook api

--- pages/api/webhooks.js
+++ pages/api/webhooks.js
@@ -2,17 +2,20 @@ import { verifyAuthIssuer } from 'solid-webhook-client';
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    console.log('Webhook request');
+    console.log('Webhook POST received');
     if (
       (await verifyAuthIssuer(req.headers.authorization)) === req.body.origin
     ) {
       console.log('Webhook valid');
       console.log(req.body);
+      return res.status(200);
     } else {
-      console.log('This issuer is invalid');
+      const m = `This authorization header of this webhook is invalid. Header ${req.headers.authorization} does not match orign ${origin}`;
+      console.log(m);
+      return res.status(401).json({
+        message: m,
+      });
     }
-
-    res.status(200);
   } else {
     return res
       .status(405)
